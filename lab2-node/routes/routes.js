8a01870e3acd3c423c244253f88690563103c33c
Mohamed-Abusaif <mohamedabusaif50@gmail.com>
2025-02-19 14:30:02 +0200

    fix read employees at home page

--- lab2-node/routes/routes.js
+++ lab2-node/routes/routes.js
@@ -4,18 +4,28 @@ const crud = require("../controllers/crud");
 const { readingTemplates } = require("./../scripts/readingTemplates");
 const { replaceTemplate } = require("./../scripts/replaceTemplate");
 
+const employeeCardTemplate = `
+  <div class="employee">
+    <h2>{%EMPLOYEE_NAME%}</h2>
+    <p>{%EMAIL%}</p>
+    <p>{%SALARY%}</p>
+    <p>ID: {%ID%}</p>
+    <p>Level: {%LEVEL%}</p>
+    <p>Experience: {%YEARS_OF_EXPERIENCE%} years</p>
+  </div>
+`;
 function routing(url, res) {
   //home
   if (url === "/") {
     res.writeHead(200, { "Content-Type": "text/html" });
-
     const home = readingTemplates().tempHome;
     const employees = crud.listEmployees();
-    const replacedTemplate = employees
-      .map((employee) => replaceTemplate(home, employee))
-      .join("");
+    console.log(employees)
 
-    res.end(replacedTemplate);
+    const employeesHTML = employees.map((employee) => replaceTemplate(employeeCardTemplate, employee)).join("");
+    const finalHTML = home.replace("{%EMPLOYEE_CARDS%}", employeesHTML);
+
+    res.end(finalHTML);
   }
   //astronomy
   else if (url === "/astronomy") {
